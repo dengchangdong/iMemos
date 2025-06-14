9d50ea09cce833d26982a245fe45a2439506d71a
dengchangdong <dengchangdong@gmail.com>
2025-06-14 18:01:42 +0800

    Version 0.0.1

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static'
 import { CONFIG } from './config.js'
 import { routes } from './routes.js'
-import { minify } from 'html-minifier'
-import { minify as terserMinify } from 'terser'
 
 /**
  * 创建Hono应用实例
@@ -25,39 +23,6 @@ app.get('/api', routes.api)
  * 静态资源处理
  */
 app.use('/*', serveStatic({ root: './' }))
-
-/**
- * 压缩HTML和JS
- * @param {string} html - 原始HTML
- * @returns {string} 压缩后的HTML
- */
-async function compressHtml(html) {
-  const minifiedHtml = minify(html, {
-    collapseWhitespace: true,
-    removeComments: true,
-    minifyJS: true,
-    minifyCSS: true,
-    preserveLineBreaks: true,
-    ignoreCustomComments: [/<pre>/, /<\/pre>/]
-  });
-  return minifiedHtml;
-}
-
-/**
- * 压缩JS
- * @param {string} js - 原始JS
- * @returns {string} 压缩后的JS
- */
-async function compressJs(js) {
-  const result = await terserMinify(js, {
-    compress: true,
-    mangle: true,
-    output: {
-      comments: false
-    }
-  });
-  return result.code;
-}
 
 /**
  * 导出应用实例
