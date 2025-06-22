6cbb493df55146fc39213fdd7509c19e696e03fe
dengchangdong <dengchangdong@gmail.com>
2025-06-22 12:22:42 +0800

    Version 0.0.2

--- src/template.js
+++ src/template.js
@@ -492,7 +492,7 @@ export function renderBaseHtml(title, content, navLinks, siteName, currentPage =
                 </button>
               </div>
             </header>
-            <main class="mt-10 relative">
+            <main class="mt-10 sm:mt-8 relative">
               ${articlesHtml}
             </main>
             
